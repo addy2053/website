ef6617cd5567b705124277cf9cde0006574a35f5
umar <addy2053@gmail.com>
2017-10-15 12:33:52 -0700

    startup

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Shadow</title>
+    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-theme.min.css">
+    <link rel="stylesheet" href="bootstrap.min.js">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <nav class="col-lg-12 nav navbar-inverse">
+            <div class="navbar-header">
+<!--            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#n">-->
+<!--                <span class="icon-bar"></span>-->
+<!--                <span class="icon-bar"></span>-->
+<!--                <span class="icon-bar"></span>-->
+<!--            </button>-->
+
+                <a href="#" class="navbar-brand">Shadow</a>
+            </div>
+            <ul class="nav navbar-nav" id="n">
+                <li><a href="">Home</a></li>
+                <li><a href="">Carrer</a></li>
+                <li><a href="">Blog</a></li>
+                <li class="dropdown">
+                    <a href="" class="dropdown-toggle" data-toggle="dropdown">Contact us</a>
+<!--                    <ul class="dropdown-menu">-->
+<!--                        <li><a href=""></a>Facebook</li>-->
+<!--                        <li><a href=""></a>Twitter</li>-->
+<!--                        <li><a href=""></a>Instagram</li>-->
+<!--                    </ul>-->
+                </li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href=""><span class="glyphicon glyphicon-log-in"></span>&nbsp;Login</a></li>
+                <li><a href=""><span class="glyphicon glyphicon-user"></span>&nbsp;Sign Up</a></li>
+            </ul>
+
+        </nav>
+    </div>
+
+
+
+
+</div>
+</body>
+</html>
