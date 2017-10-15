8fc0c6de469f48eea09d33aaae1bd32e40146482
umar <addy2053@gmail.com>
2017-10-15 12:57:24 -0700

    navbar

--- home.php
+++ home.php
@@ -10,35 +10,38 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container-fluid">
     <div class="row">
-        <nav class="col-lg-12 nav navbar-inverse">
-            <div class="navbar-header">
-<!--            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#n">-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--            </button>-->
+        <nav class="col-md-12 nav navbar-inverse">
+            <div class="navbar-header ">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#n">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
                 <a href="#" class="navbar-brand">Shadow</a>
             </div>
+
             <ul class="nav navbar-nav" id="n">
-                <li><a href="">Home</a></li>
+                <li><a href=""><span class="glyphicon glyphicon-home"> </span></a></li>
                 <li><a href="">Carrer</a></li>
                 <li><a href="">Blog</a></li>
                 <li class="dropdown">
                     <a href="" class="dropdown-toggle" data-toggle="dropdown">Contact us</a>
-<!--                    <ul class="dropdown-menu">-->
-<!--                        <li><a href=""></a>Facebook</li>-->
-<!--                        <li><a href=""></a>Twitter</li>-->
-<!--                        <li><a href=""></a>Instagram</li>-->
-<!--                    </ul>-->
+                    <ul class="dropdown-menu nav navbar">
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                    </ul>
                 </li>
             </ul>
-
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="nav navbar-nav navbar-right" id="n">
                 <li><a href=""><span class="glyphicon glyphicon-log-in"></span>&nbsp;Login</a></li>
                 <li><a href=""><span class="glyphicon glyphicon-user"></span>&nbsp;Sign Up</a></li>
             </ul>
