848a6472bd3295df6a77df77115a1e21e6735d39
umar <addy2053@gmail.com>
2017-10-16 13:19:49 -0700

    aaa

--- home.php
+++ home.php
@@ -15,9 +15,12 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container-fluid">
+<div id="body">
+
+</div>
+<div class="container-fluid" style="  background-image: image("dragon.jpg")">
     <div class="row">
-        <nav class="col-md-12 nav navbar-inverse">
+        <nav class="col-md-12 nav navbar">
             <div class="navbar-header ">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#n">
                 <span class="icon-bar"></span>
@@ -29,13 +32,13 @@
             </div>
 
             <ul class="nav navbar-nav" id="n">
-                <li><a href=""><span class="glyphicon glyphicon-home"> </span></a></li>
+                <li><a href="">Home</a></li>
                 <li><a href="">Carrer</a></li>
                 <li><a href="">Blog</a></li>
                 <li class="dropdown">
                     <a href="" class="dropdown-toggle" data-toggle="dropdown">Contact us</a>
-                    <ul class="dropdown-menu nav navbar">
-                        <li><a href="#">Facebook</a></li>
+                    <ul id="sub" class="dropdown-menu navbar-default">
+                        <li><a href="#"></>Facebook</a></li>
                         <li><a href="#">Twitter</a></li>
                         <li><a href="#">Instagram</a></li>
                     </ul>
@@ -48,10 +51,54 @@
 
         </nav>
     </div>
+    <div class="row">
+         <div class="jumbotron text-center  col-md-8 col-md-offset-2">
+             <p >sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+             sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+             sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+             sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df</p>
+             <button type="button" class="btn btn-group-justified" onclick="disp()">Read more...</button>
+         </div>
+    </div>
+<div class="row col-md-offset-2" id="col">
+    <div class="col-md-3">
+        <p >sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df</p>
+        <button type="button" class="btn btn-danger" onclick="disp()">Read more...</button>
+    </div>
+    <div class="col-md-3">
+        <p >sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df</p>
+        <button type="button" class="btn btn-danger" onclick="disp()">Read more...</button>
+    </div>
+    <div class="col-md-3">
+        <p >sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            sadsa da dsadsa sdgfg d ghgh fdgjhkj hkh jfhfdg gjf gdfjhbgdfkg fdkfdjg fdkjg fdjkg fdkjg dfjkgdfkjg df
+            <button type="button" class="btn btn-danger" onclick="disp()">Read more...</button></p>
+    </div>
 
 
 
 
 </div>
+<footer class="panel panel-success">
+    <div class="col-md-6 col-md-offset-4">
+        <p><a href="gg" class="navbar-text">Give your feed back about this shadow.com</a></p>
+        <p style="color: #555555" class="navbar-right navbar-text">Copywrite by Umar javaid</p>
+    </div>
+
+</footer>
+</div>
 </body>
+
 </html>
